e10ff317bb299d2d0adab9983753a248e128b913
Dmitry Litvintsev <litvinse@fnal.gov>
2016-05-05 15:25:08 -0500

    Script that takes CMS /etc/grid-security/grid-vorolemap and /etc/grid-security/storage-authzdb to generate reduced version that contains only
    * FQAN username
    
    per line

---
+++ scripts/cms_vorolemap_converter.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+"""
+massage CMS /etc/grid-security/grid-vorolemap and /etc/grid-security/storage-authzdb
+to generate reduced version that contains only 
+
+* FQAN username
+
+per line
+
+"""
+
+AUTHZDB_FILE="/etc/grid-security/storage-authzdb"
+VOROLEMAP_FILE="/etc/grid-security/grid-vorolemap"
+
+import sys
+import shlex
+
+if __name__ == "__main__":
+
+    vorolemap={}
+    with open(VOROLEMAP_FILE,"r") as f:
+        for line in f:
+            if not line : continue
+            parts = shlex.split(line.strip())
+            """
+            ignore entries that are missing FQAN
+            """
+            if len(parts) != 3 : continue
+
+            fqan     = parts[1]
+            username = parts[2]
+            """
+            only grab enstries that contain 'cms' in FQAN string
+            """
+            if fqan.find("cms") != -1:
+                vorolemap[fqan] = username
+    f.closed
+
+    authzdb={}
+    with open(AUTHZDB_FILE,"r") as f:
+        for line in f:
+            if not line : continue
+            parts = line.strip().split()
+            username   = parts[1]
+            rw         = line.strip()
+            if username in authzdb :
+                print "already there, but will take last ", line.strip(), " vs ", authzdb[username]
+            authzdb[username] = line.strip()
+    f.closed
+
+    items = vorolemap.items()
+    items.sort(key=lambda x: x[0])
+
+    """
+    write out new grid-vorolemap
+    """
+    new_authzdb = {}
+    with open("grid-vorolemap.new","w") as f:
+        for (fqan, username) in items:
+            if username in authzdb:
+                f.write("\"*\" \"%s\" %s\n"%(fqan,username))
+                new_authzdb[username] = authzdb[username]
+
+    f.closed
+
+    items = new_authzdb.items()
+    items.sort(key=lambda x: x[0])
+
+
+    """
+    write out new storage-authzdb
+    """
+    
+    with open("storage-authzdb.new","w") as f:
+        for (username,entry) in items:
+            f.write("%s\n"%(entry,))
+    f.closed
+
+
+
+
